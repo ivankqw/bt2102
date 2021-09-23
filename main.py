6607f54035cdc1a70f811d871f502961289ef34a
etbf <ernest-tan123@hotmail.com>
2021-09-24 00:28:08 +0800

    tried to put approve button in approve home page
    waiting for valentin's code, as of now approve button changes the approve, but doesnt alter the table in mySQL

--- main.py
+++ main.py
@@ -688,6 +688,14 @@ def CustomerHomePage(root, cursor):
     return 
 
 def ApproveHomePage(root, cursor):
+    def approve_request():
+        selected = tree.focus()
+        temp = tree.item(selected, 'values')
+        approve = "Approved"
+        tree.item(selected, values=(temp[0], temp[1], approve, temp[3], temp[4], temp[5]))
+        sql_statement = "UPDATE request SET requestStatus = 'Approved' WHERE requestID = '%s')"
+        cursor.execute(sql_statement, temp[0])
+
     main_screen = root    
     main_screen.title("OSHES app") 
     main_screen.config(bg='#0B5A81')  
@@ -697,10 +705,12 @@ def ApproveHomePage(root, cursor):
     selection_statement = "SELECT * FROM request WHERE requestStatus = 'In Progress'"
     cursor.execute(selection_statement)
     table_info = cursor.fetchall()
+    cursor.reset()
 
     style = ttk.Style()
     style.theme_use('default')
-    tree = ttk.Treeview(root, columns = ('Request ID', 'Date of Request', 'Request Status', 'Customer ID', 'Admin ID', 'Item ID'), show = 'headings')
+    tree = ttk.Treeview(root, columns = ('Request ID', 'Date of Request', 'Request Status', 'Customer ID', 'Admin ID', 'Item ID', 'Approve?'), show = 'headings')
+    tree.pack()
 
     root.title('Approval Page')
     tree.column('#1', anchor = CENTER, width = '100')
@@ -721,7 +731,7 @@ def ApproveHomePage(root, cursor):
     else:
         for i in table_info:
             tree.insert("", "end", values = i)
-        tree.pack()
+        tkinter.Button(text = 'Approve', command = approve_request).pack()
 
     tkinter.Label(text="", bg='#0B5A81').pack()  
     tkinter.Button(text="Back To Admin Home Page", height="2", width="30", bg="yellow", relief=tkinter.SOLID,cursor='hand2',command= lambda: changepage("adminHomePage")).pack(side=tkinter.BOTTOM)
